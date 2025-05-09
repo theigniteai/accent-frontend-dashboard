bbf93e1f615daaf37128e69f10ae8cc1af9a661f
theigniteai <theignite.ai@gmail.com>
2025-05-10 00:44:52 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,41 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import ForgotPassword from "./pages/ForgotPassword";
-import Profile from "./pages/Profile";
-import Settings from "./pages/Settings";
-import Billing from "./pages/Billing";
-import Subscription from "./pages/Subscription";
-import Pricing from "./pages/Pricing";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+
 import PrivateRoute from "./routes/PrivateRoute";
-import AuthProvider from "./context/AuthContext";
 import DashboardLayout from "./layouts/DashboardLayout";
 
-const App = () => {
-  return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/pricing" element={<Pricing />} />
+const App = () => (
+  <Router>
+    <Navbar />
 
-          {/* Protected Dashboard Routes */}
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <DashboardLayout />
-              </PrivateRoute>
-            }
-          >
-            <Route index element={<Dashboard />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="billing" element={<Billing />} />
-            <Route path="subscription" element={<Subscription />} />
-          </Route>
-        </Routes>
-        <Footer />
-      </Router>
-    </AuthProvider>
-  );
-};
+    <Routes>
+      {/* Public */}
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      {/* All dashboard routes under one layout */}
+      <Route
+        path="/dashboard/*"
+        element={
+          <PrivateRoute>
+            <DashboardLayout />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+
+    <Footer />
+  </Router>
+);
 
 export default App;
