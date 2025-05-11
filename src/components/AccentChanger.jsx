5f8113c2be48f5494c4a3de32aa8f3938526cca1
theigniteai <theignite.ai@gmail.com>
2025-05-11 18:35:14 +0400

    Update AccentChanger.jsx

--- src/components/AccentChanger.jsx
+++ src/components/AccentChanger.jsx
@@ -9,7 +9,7 @@ const AccentChanger = () => {
   const startRecording = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-    const ws = new WebSocket("wss://accent-backend-final.onrender.com");
+    const ws = new WebSocket("wss://accentshift-relay.up.railway.app");
     ws.binaryType = "arraybuffer";
 
     ws.onopen = () => {
